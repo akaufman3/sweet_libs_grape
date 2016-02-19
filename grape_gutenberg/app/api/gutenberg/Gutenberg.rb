fece6332122a0520ca583e2d13bc9befcfef6e3d
Zachary Boyd <zachary@zacharyboyd.nyc>
2016-02-19 16:23:51 -0500

    intial

---
+++ grape_gutenberg/app/api/gutenberg/Gutenberg.rb
@@ -0,0 +1,29 @@
+module Gutenberg
+	class API < Grape::API
+		version 'v1', using: :header, vendor: 'ga-student'
+		format :json
+		prefix :book
+		
+		get '/:number' do
+			number = params[:number].to_i
+
+			redis ||= Redis.new(:host => 'localhost', :port => 6379)
+
+    	path = open("../epub/#{number}/pg#{number}.rdf")
+    	cached_data = redis.get(number)
+    	if cached_data == nil
+	      doc = Nokogiri::XML(path)
+	      variant = doc.xpath('/rdf:RDF/xml:base')
+	      book = {
+	      	title: doc.xpath('/rdf:RDF/pgterms:ebook/dcterms:title')[0].text
+	      }
+
+	      redis.set(number, JSON.generate(book))
+
+	      return book
+	    else
+	    	return JSON.parse(cached_data)
+	    end
+		end
+	end
+end
